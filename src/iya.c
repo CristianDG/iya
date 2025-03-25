edcc40367393da468600d48d4a39bc3ce0c46d59
CristianDG <39601699+CristianDG@users.noreply.github.com>
2025-03-25 14:40:50 -0300

    definição do modelo

--- src/iya.c
+++ src/iya.c
@@ -4,16 +4,30 @@
 #define DG_MATRIX_IMPLEMENTATION
 #include "cdg_base.c"
 
+#define Slice_Of_Slice(type) Make_Slice_Type(Make_Slice_Type(type))
+
+typedef struct {
+  u32 a;
+  u32 b;
+} Connection;
+
+typedef struct {
+  Make_Slice_Type(f32) inputs;
+  Make_Slice_Type(f32) outputs;
+  Slice_Of_Slice(f32) ws;
+  Slice_Of_Slice(f32) bs;
+  Slice_Of_Slice(Connection) connections;
+} ML_Model;
+
 Arena permanent_arena = {};
 
 int main(void)
 {
   {
-    usize size = 2 * KILOBYTE;
+    usize size = 2 * MEGABYTE;
     u8 *mem = malloc(size);
     permanent_arena = arena_init_buffer(mem, size);
   }
-
 
   return 69;
 }
