d7299dd7a95f2b251b640536a42af2742edcac02
CristianDG <39601699+CristianDG@users.noreply.github.com>
2025-04-03 11:01:26 -0300

    melhorando build.sh

--- build.sh
+++ build.sh
@@ -5,12 +5,16 @@ set -euo pipefail
 
 flags=""
 flags+=" -std=c11"
+
+flags+=" -Wall -Werror"
+flags+=" -Wno-unused-variable"
+
 flags+=" -DDG_PLATFORM_WASM"
 flags+=" -Ithird_party"
 flags+=" -g -fno-builtin --no-standard-libraries"
 flags+=" -gdwarf-5 -gpubnames"
-# flags+=" -gsplit-dwarf"
 flags+=" --target=wasm32-freestanding "
+# flags+=" -gsplit-dwarf"
 
 linker_flags=""
 linker_flags+=" -Wl,--no-entry -Wl,--export=__heap_base -Wl,--export=__heap_end"
